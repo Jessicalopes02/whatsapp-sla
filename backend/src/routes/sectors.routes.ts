cf4ded1e72a76926a266bc0739dad275b603cd8d
Jessica Lopes <111246373+Jessicalopes02@users.noreply.github.com>
2026-07-10 16:53:43 -0300

    Update sectors.routes.ts

--- backend/src/routes/sectors.routes.ts
+++ backend/src/routes/sectors.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { SectorsController } from "../controllers/sectors.controller";
 
 export const sectorsRoutes = Router();
+
 const controller = new SectorsController();
 
 sectorsRoutes.get("/", controller.list);
+sectorsRoutes.post("/", controller.create);
